c365970d4d952727ece5c106190b25b7e8dfd2a3
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-04-17 21:42:29 +0100

    add auth, user ans posts controllers

---
+++ controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import * as authService from "../services/auth.service.js";
+
+//Register user
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const newUser = await authService.registerUser(name, email, password);
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: newUser });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Registration failed", error: err.message });
+  }
+};
+
+
+
+//Login user
+export const loginUser = async (req, res) => {
+  try {
+
+    const { email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const {token, user} = await authService.login(email, password);
+
+    res.json({
+      message: "Login successful",
+      data: {
+        accessToken: token,
+      },
+      user: user
+    });
+
+  } catch (err) {
+    res.status(err.status || 500);
+    res.json({ message: err.message });
+  }
+};
